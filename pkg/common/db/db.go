8831664f6bfc445dc3ca6489120a05bd0b02667b
Araks <lebedevartemiy74@gmail.com>
2025-01-18 15:49:19 +0500

    Попытка номер 2

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -1,1 +1,19 @@
 package db
+
+import (
+	"log"
+
+	"github.com/Araks1255/libraryofsongs"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
--- pkg/common/models/song.go
+++ pkg/common/models/song.go
@@ -1,1 +1,12 @@
 package models
+
+import "gorm.io/gorm"
+
+type Song struct {
+	gorm.Model
+	Name          string `json:"name"`
+	Band          string `json:"band"`
+	Album         string `json:"album"`
+	Genre         string `json:"genre"`
+	YearOfRelease string `json:"yearOfRelease"`
+}
