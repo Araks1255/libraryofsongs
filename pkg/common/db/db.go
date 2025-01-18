d949581f25a9f214908931233ed9379a021d9d55
Araks <lebedevartemiy74@gmail.com>
2025-01-18 16:01:45 +0500

    Попытка номер 4

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -3,16 +3,19 @@ package db
 import (
 	"log"
 
-	"gorm.io/gorm"
+	"gitub.com/Araks1255/libraryofsongs/pkg/common/models"
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func Init(url string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(url), &gorm.Config)
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	db.AutoMigrate(&models.Song{})
 
 	return db
 }
